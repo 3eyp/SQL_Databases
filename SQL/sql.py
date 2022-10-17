032feb6062b35880dfd1598ea6e1d8bc828b417c
eyüp <class.py12@gmail.com>
2022-10-17 15:50:29 +0300

    PyQt5 and DATA
    Programda manuel olarak profesyonel bir şekilde veri ekleme, güncelleme, silme, taşıma, sıralama yapılabilmektedir ayrıca pc'nizde kurulu olan MySQL workbench servere bağlanıp oradan direkt olarak istenilen veriler çekilebilmektedir.Buradan çekilen verilerde veri kaybının önüne geçilmiştir.
    Yazdığım programı terminalden output etmek yerine arayüz haline PyQt5 ile göze güzel hitap eden bir hale getirdim.
    Tüm butonlar aktif ve hataya yer bırakılmadı.
    Ayrıca resim dosyaları gömülü hale getirildi

--- SQL/sql.py
+++ SQL/sql.py
@@ -44,16 +44,12 @@ class MyApp(QtWidgets.QMainWindow):
         self.ui.btn_asagi.setStyleSheet("border-radius:8px;background-color:qlineargradient(spread:pad, x1:0.165, y1:0.840909, x2:1, y2:0, stop:0 rgba(0, 43, 107, 255), stop:1 rgba(255, 255, 255, 255));color:rgb(255, 170, 0);font: 75 10pt 'MS Shell Dlg 2';color: rgb(210, 252, 255);")
         self.ui.btn_sil.setStyleSheet("border-radius:8px;background-color:qlineargradient(spread:pad, x1:0.165, y1:0.840909, x2:1, y2:0, stop:0 rgba(0, 43, 107, 255), stop:1 rgba(255, 255, 255, 255));color:rgb(255, 170, 0);font: 75 10pt 'MS Shell Dlg 2';color: rgb(210, 252, 255);")
         self.ui.btn_exit.setStyleSheet("border-radius:8px;background-color:qlineargradient(spread:pad, x1:0.165, y1:0.840909, x2:1, y2:0, stop:0 rgba(0, 43, 107, 255), stop:1 rgba(255, 255, 255, 255));color:rgb(255, 0, 0);font: 75 12pt 'MS Shell Dlg 2';color: rgb(176, 249, 255);")
-        self.ui.btn_veriCek.setStyleSheet("border-radius:8px;background-color:qlineargradient(spread:pad, x1:0.165, y1:0.840909, x2:1, y2:0, stop:0 rgba(0, 43, 107, 255), stop:1 rgba(255, 255, 255, 255));color:rgb(255, 170, 0);font: 75 10pt 'MS Shell Dlg 2';color: rgb(210, 252, 255);")
-        self.ui.pushButton_7.setStyleSheet("border-radius:8px;background-color:qlineargradient(spread:pad, x1:0.165, y1:0.840909, x2:1, y2:0, stop:0 rgba(0, 43, 107, 255), stop:1 rgba(255, 255, 255, 255));color:rgb(255, 170, 0);font: 75 10pt 'MS Shell Dlg 2';color: rgb(210, 252, 255);")
         self.ui.btn_sirala.setStyleSheet("border-radius:8px;background-color:qlineargradient(spread:pad, x1:0.165, y1:0.840909, x2:1, y2:0, stop:0 rgba(0, 43, 107, 255), stop:1 rgba(255, 255, 255, 255));color:rgb(255, 170, 0);font: 75 10pt 'MS Shell Dlg 2';color: rgb(210, 252, 255);")
 
         self.item()
 
         self.ui.btn_ekle.clicked.connect(self.ekle)
         self.ui.btn_sil.clicked.connect(self.sil)
-        self.ui.pushButton_7.clicked.connect(self.veriGonder)
-        self.ui.btn_veriCek.clicked.connect(self.vericek)
         self.ui.btn_guncelle.clicked.connect(self.guncelle)
         self.ui.btn_yukari.clicked.connect(self.yukari)
         self.ui.btn_asagi.clicked.connect(self.asagi)
@@ -109,44 +105,13 @@ class MyApp(QtWidgets.QMainWindow):
         index = self.ui.listWidget_data.currentRow()
         item=self.ui.listWidget_data.takeItem(index)
         if index and item is None:
-            QMessageBox.question(self,"Error","Veri seçilmedi",QMessageBox.Ok)
+            QMessageBox.question(self,"Error","Veri bulunamadı",QMessageBox.Ok)
         else:
             ok=QMessageBox.question(self,"Deleted",f"{item.text()} kişisini silmek istediğinizden emin misiniz",QMessageBox.Ok,QMessageBox.Cancel)
             if ok==QMessageBox.Ok:
                 del item
             else:
                 self.ui.listWidget_data.insertItem(index,item.text())
-
-    def veriGonder(self):
-        connect = mysql.connector.connect(
-            host="localhost", # Bu alana MySQL serverinizin host alanını giriniz
-            user="root",      # Bu alana MySQL serverinizin user alanını giriniz
-            password="eyupeser", # Bu alana MySQL serverinizin parolasını giriniz
-            database="yenicalismam" # Bu alana MySQL serveriniz içerisinde çalışma yapmak istediğiniz verilerin olduğu alanı giriniz
-        )
-        cursor = connect.cursor()
-        sql = "Select ad,soyad from bilgi" # 'bilgi' yazan yere verilerini görmek istediğiniz alanı giriniz. 'ad' ve 'soyad' yazan yerler yerine istediğiniz kolon isimlerini giriniz
-        cursor.execute(sql)
-        result1=[i[0].__str__().capitalize() for i in cursor.fetchall()] # Burada girdiğiniz kolonları bölmek istediğiniz şekilde for ile ayarlayınız
-
-        a=len(result1)
-
-        if self.ui.listWidget_sql.count() == a:
-            QMessageBox.question(self,"Message","Veriler aktarıldı.",QMessageBox.Ok)
-        else:
-            self.ui.lbl_sql.setText("SQL veriler")
-            self.ui.listWidget_sql.addItems(result1)
-
-    def vericek(self):
-        index=self.ui.listWidget_sql.currentRow()
-
-        if index is None:
-            QMessageBox.question(self, "Error", "Veri çekmek içi Veri Gör butonunu seçiniz", QMessageBox.Ok)
-        else:
-            if index is not None:
-                item = self.ui.listWidget_sql.takeItem(index)
-                self.ui.listWidget_data.insertItem(index,item)
-                self.ui.listWidget_sql.insertItem(index,item.text())
 
     def sirala(self):
         self.ui.listWidget_data.sortItems()
